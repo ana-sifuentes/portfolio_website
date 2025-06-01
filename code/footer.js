0092431cd155f587f4908dda609fdced8a532790
ana sifuentes <sifuentesanita@gmail.com>
2025-06-01 11:19:22 +0200

    updates

--- code/footer.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  fetch("footer.html")
-    .then(res => res.text())
-    .then(data => {
-      document.getElementById("footer-placeholder").innerHTML = data;
-
-    });
-});
