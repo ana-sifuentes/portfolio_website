d47e7148c58282e0cdc3d153c22212c21808687c
ana sifuentes <sifuentesanita@gmail.com>
2025-06-05 13:01:35 +0200

    favicon and meny

--- code/menu-footer.js
+++ code/menu-footer.js
@@ -19,45 +19,46 @@ $(document).ready(function () {
 
       lastScrollY = window.scrollY;
     });
-  });
 
-  // Load menu
-  $("#menu-placeholder").load("/components/menu.html", function () {
-    const $hamburger = $(".hamburger");
-    const $navitem = $(".nav-item");
-    const $menu = $("#menu");
-    const $body = $("body");
+    // Load menu
+    $("#menu-placeholder").load("/components/menu.html", function () {
+      const $hamburger = $(".hamburger");
+      const $navitem = $(".nav-item");
+      const $menu = $("#menu");
+      const $body = $("body");
 
-    function toggleMenu(forceState = null) {
-      const isHidden = $menu.hasClass("hidden");
-      const shouldOpen = forceState !== null ? forceState : isHidden;
+      function toggleMenu(forceState = null) {
+        const isHidden = $menu.hasClass("hidden");
+        const shouldOpen = forceState !== null ? forceState : isHidden;
 
-      if (shouldOpen) {
-        $menu.removeClass("hidden");
-        $body.addClass("fixed-position");
-        $hamburger.addClass("active");
-        $navitem.addClass("hidden");
-      } else {
-        $menu.addClass("hidden");
-        $body.removeClass("fixed-position");
-        $hamburger.removeClass("active");
-        $navitem.removeClass("hidden");
+        if (shouldOpen) {
+          $menu.removeClass("hidden");
+          $body.addClass("fixed-position");
+          $hamburger.addClass("active");
+          $navitem.addClass("hidden");
+        } else {
+          $menu.addClass("hidden");
+          $body.removeClass("fixed-position");
+          $hamburger.removeClass("active");
+          $navitem.removeClass("hidden");
+        }
       }
-    }
 
-    // Toggle on hamburger click
-    $hamburger.on("click", function (e) {
-      e.stopPropagation(); // Prevent bubbling to document
-      toggleMenu();
-    });
+      // Toggle on hamburger click
+      $hamburger.on("click", function (e) {
+        e.stopPropagation(); // Prevent bubbling to document
+       // console.log("change to x");
+        toggleMenu();
+      });
 
-    // Click outside of the .grid (but inside #menu) should close menu
-    $menu.on("click", function (e) {
-      const $grid = $menu.find(".grid");
+      // Click outside of the .grid (but inside #menu) should close menu
+      $menu.on("click", function (e) {
+        const $grid = $menu.find(".grid");
 
-      if (!$grid.is(e.target) && $grid.has(e.target).length === 0) {
-        toggleMenu(false);
-      }
+        if (!$grid.is(e.target) && $grid.has(e.target).length === 0) {
+          toggleMenu(false);
+        }
+      });
     });
   });
 
